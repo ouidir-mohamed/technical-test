b9b8018ccaec1881a2a34c6be6b3616bbbae20b2
ouidir-mohamed <amedhom@gmail.com>
2023-04-27 17:00:39 +0200

    fix loading sql before test

--- src/test/java/com/example/technicaltest/it/services/CountryServiceTest.java
+++ src/test/java/com/example/technicaltest/it/services/CountryServiceTest.java
@@ -6,7 +6,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -15,6 +17,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Sql(scripts = "/data.sql")
 public class CountryServiceTest {
 
     @Autowired
